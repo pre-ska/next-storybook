300a0835b2f7de47414b97162ac1dfb85153dae7
pskaro <wolfwar@outlook.com>
2023-10-10 15:53:51 +0200

    feat: search page UI done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import PrimaryLayout from "@/components/layouts/primary/PrimaryLayout";
 import Search from "@/components/utility/search/Search";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { NextPageWithLayout } from "./page";
 
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
+
   return (
-    <section>
-      logo
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <Image
+        src="/Google.png"
+        alt="Google Logo"
+        width={272}
+        height={92}
+        priority
+      />
       <Search />
-      language tools
+      <p>
+        Google offered in:{" "}
+        <Link
+          href="/"
+          locale={locale === "en" ? "fr" : "en"}
+          className="underline text-blue-600"
+        >
+          Français
+        </Link>
+      </p>
     </section>
     // <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
     //   <h1>Welcome to bla bla!</h1>
